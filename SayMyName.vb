a458c3668404bc7bcb2260b8dc627c46d383b2f5
unknown <dalekrebel@gmail.com>
2024-01-18 08:09:25 -0700

    First Commit

---
+++ SayMyName.vb
@@ -0,0 +1,21 @@
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'SayYourName
+'https://github.com/Masaharu41/SayMyName.git
+
+Option Explicit On
+Option Strict On
+Module SayMyName
+
+    Sub Main()
+        Dim userName As String
+        Console.Write("What is your name?")
+        Console.WriteLine()
+        userName = Console.ReadLine()
+        Console.Write($"Pleasure to meet you {userName}")
+
+        Console.Read()
+    End Sub
+
+End Module
